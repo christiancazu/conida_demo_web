9d0850d23cb28b115bc3a14a47c5b010995c1196
William Condori Quispe <williamccondori@gmail.com>
2020-01-04 10:28:52 -0500

    Add projects

---
+++ layouts/admin.vue
@@ -0,0 +1,25 @@
+<template>
+<el-container>
+  <el-aside
+    width="300px"
+    :class="{ 'app-aside': true}"
+  >
+    <sidebar />
+  </el-aside>
+  <el-container>
+    <el-header />
+    <el-main>
+      <nuxt />
+    </el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+import sidebar from '~/components/admin/sidebar.vue'
+export default {
+  components: {
+    sidebar
+  }
+}
+</script>
