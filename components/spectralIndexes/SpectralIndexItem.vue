45be439ef8288d501f980db129a641f7ecb6d69e
mariley <mariley2018@gmail.com>
2020-01-06 12:01:52 -0500

    added autocompleted input for satellite images

--- components/spectralIndexes/SpectralIndexItem.vue
+++ components/spectralIndexes/SpectralIndexItem.vue
@@ -1,5 +1,28 @@
 <template>
 <el-main>
+  <!-- selected image -->
+  <el-autocomplete
+    v-model="searchImage"
+    :fetch-suggestions="querySearch"
+    placeholder="Selecciona una imagen del proyecto"
+    clearable
+    @select="selectedImage"
+  >
+    <template slot-scope="{ item }">
+      <div class="scope-item">
+        <el-avatar
+          class="item-image"
+          shape="square"
+          size="medium"
+          src="https://apps.sentinel-hub.com/sentinel-playground/previews/b0eec84c-1-NATURAL-COLOR.jpeg"
+        />
+        <span class="item-content">
+          {{ item.value }}
+        </span>
+      </div>
+    </template>
+  </el-autocomplete>
+  <!-- sacar component card -->
   <el-card>
     <div class="card-spectral-index">
       <el-avatar
@@ -8,7 +31,7 @@
         src="https://apps.sentinel-hub.com/sentinel-playground/previews/b0eec84c-1-NATURAL-COLOR.jpeg"
       />
       <div class="content">
-        <h5>INDICE NDVI</h5>
+        <h5 class="content__title">INDICE NDVI</h5>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
     </div>
@@ -24,7 +47,33 @@ export default {
 
   data: () => ({
     // defined if mountableAsDynamicMixin is included
-    dynamicTitle: 'TEST title'
-  })
+    dynamicTitle: 'Indices espectrales',
+    searchImage: '',
+    images: [
+      { "value": "vue", "link": "https://github.com/vuejs/vue" },
+      { "value": "element", "link": "https://github.com/ElemeFE/element" },
+      { "value": "cooking", "link": "https://github.com/ElemeFE/cooking" },
+      { "value": "mint-ui", "link": "https://github.com/ElemeFE/mint-ui" },
+      { "value": "vuex", "link": "https://github.com/vuejs/vuex" },
+      { "value": "vue-router", "link": "https://github.com/vuejs/vue-router" },
+      { "value": "babel", "link": "https://github.com/babel/babel" }
+    ]
+  }),
+
+  methods: {
+    querySearch (string, cb) {
+      let results = this.images
+      if (string) {
+        const stringToLowerCase = string.toLowerCase()
+        results = this.images.filter(item => {
+          return item.value.toLowerCase().includes(stringToLowerCase)
+        })
+      }
+      cb(results)
+    },
+    selectedImage (item) {
+      console.log(item)
+    }
+  },
 }
 </script>
