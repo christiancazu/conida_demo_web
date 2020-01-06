83c4d2b7f07aa36b878fe5151a9dd06770274fe0
mariley <mariley2018@gmail.com>
2020-01-06 00:28:36 -0500

    added card spectral indexes

---
+++ components/spectralIndexes/SpectralIndexItem.vue
@@ -0,0 +1,30 @@
+<template>
+<el-main>
+  <el-card>
+    <div class="card-spectral-index">
+      <el-avatar
+        class="image"
+        :size="70"
+        src="https://apps.sentinel-hub.com/sentinel-playground/previews/b0eec84c-1-NATURAL-COLOR.jpeg"
+      />
+      <div class="content">
+        <h5>INDICE NDVI</h5>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  </el-card>
+</el-main>
+</template>
+
+<script>
+import mountableAsDynamicMixin from "@/mixins/mountableAsDynamic.mixin"
+
+export default {
+  mixins: [mountableAsDynamicMixin],
+
+  data: () => ({
+    // defined if mountableAsDynamicMixin is included
+    dynamicTitle: 'TEST title'
+  })
+}
+</script>
